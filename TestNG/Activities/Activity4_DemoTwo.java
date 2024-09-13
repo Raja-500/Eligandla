519230039e0b08d92a9d86a504b6f0d0aa06b218
Eligandlaraja/FST-M1 <Eligandla.Raja@ibm.com>
2024-09-13 18:12:21 +0530

    Delete TestNG/Activities/Activity4_DemoTwo.java

--- TestNG/Activities/Activity4_DemoTwo.java
+++
@@ -1,8 +0,0 @@
-import org.testng.annotations.Test;
-
-public class Activity4_DemoTwo {
-    @Test
-    public void TestCase() {
-        System.out.println("I'm in the test case from DemoTwo Class");
-    }
-}
